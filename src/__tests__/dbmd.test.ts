18510598e97b926b3e6adf45f164cef827b13e3c
Kevin Frei <kevinfrei@hotmail.com>
2021-05-02 21:37:18 -0700

    Adding some testing for dbMetadata

--- src/__tests__/dbmd.test.ts
+++ src/__tests__/dbmd.test.ts
@@ -1,4 +1,4 @@
-/* 
+/*
 export declare function getMediaInfo(mediaPath: string): Promise<Map<string, string>>;
 export declare type MetadataStore = {
     get: (path: string) => MinimumMetadata | void;
@@ -11,8 +11,10 @@ export declare type MetadataStore = {
 export declare function GetMetadataStore(persist: Persist, name: string): Promise<MetadataStore>;
 */
 
+import { Type } from '@freik/core-utils';
 import { FullMetadata } from '@freik/media-core';
-import { IsFullMetadata, IsOnlyMetadata } from '../DbMetadata';
+import { promises as fsp } from 'fs';
+import { getMediaInfo, IsFullMetadata, IsOnlyMetadata } from '../DbMetadata';
 
 it('Full/Partial Metadata tests', () => {
   const partial = { originalPath: '/a/file/path.mp3', track: 1 };
@@ -38,4 +40,44 @@ it('Full/Partial Metadata tests', () => {
   const extras = { notOK: false, ...aFewMore };
   expect(IsOnlyMetadata(extras)).toBe(false);
   expect(IsFullMetadata(extras)).toBe(false);
+});
+
+it('Generic getMediaInfo tests', async () => {
+  const data = await fsp.readFile('src/__tests__/dbmdData.json');
+  const dataParse = JSON.parse(data.toString());
+
+  const flacMap = new Map<string, string>(dataParse['flac']);
+  const flacFile =
+    'src/__tests__/metadata/Album - 2005 - Artist/01 - quiet.flac';
+  const miFlac = await getMediaInfo(flacFile);
+  expect(Type.isMap(miFlac)).toBeTruthy();
+  expect(miFlac).toEqual(flacMap);
+
+  const m4aMap = new Map<string, string>(dataParse['m4a']);
+  const m4aFile =
+    'src/__tests__/metadata/Album - 2005 - Artist/02 - quiet2.m4a';
+  const mim4a = await getMediaInfo(m4aFile);
+  expect(Type.isMap(mim4a)).toBeTruthy();
+  expect(mim4a).toEqual(m4aMap);
+
+  const mp3Map = new Map<string, string>(dataParse['mp3']);
+  const mp3File =
+    'src/__tests__/metadata/Album - 2005 - Artist/03 - Quiet3.mp3';
+  const mimp3 = await getMediaInfo(mp3File);
+  expect(Type.isMap(mimp3)).toBeTruthy();
+  expect(mimp3).toEqual(mp3Map);
+
+  /*  const flacMap = new Map<string, string>(dataParse);
+  const flacFile =
+    'src/__tests__/metadata/Album - 2005 - Artist/01 - quiet.flac';
+  const miFlac = await getMediaInfo(flacFile);
+  expect(Type.isMap(miFlac)).toBeTruthy();
+  expect(miFlac).toEqual(flacMap);
+
+  const flacMap = new Map<string, string>(dataParse);
+  const flacFile =
+    'src/__tests__/metadata/Album - 2005 - Artist/01 - quiet.flac';
+  const miFlac = await getMediaInfo(flacFile);
+  expect(Type.isMap(miFlac)).toBeTruthy();
+  expect(miFlac).toEqual(flacMap);*/
 });
