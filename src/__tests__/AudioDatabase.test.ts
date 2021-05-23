418a2287e96a97d2b930ef8a5e260f591e8e881a
Kevin Frei <kevinfrei@hotmail.com>
2021-05-23 13:23:55 -0700

    Full internal consistency checks for audio database

--- src/__tests__/AudioDatabase.test.ts
+++ src/__tests__/AudioDatabase.test.ts
@@ -1,3 +1,5 @@
+import { ArrayIntersection } from '@freik/core-utils/lib/Operations';
+import { Album, Artist } from '@freik/media-core';
 import { MakePersistence } from '@freik/node-utils';
 import fs from 'fs';
 import { MakeAudioDatabase } from '../AudioDatabase';
@@ -37,12 +39,103 @@ it('dummy test', async () => {
   expect(true).toBeTruthy();
 });
 
-it('make a big database', async () => {
+it('Querty a reasonably sized database', async () => {
   const db = await MakeAudioDatabase(persist);
+  expect(db).toBeDefined();
   const afi = await MakeAudioFileIndex('./src/__tests__/NotActuallyFiles', 0);
+  expect(afi).toBeDefined();
   await afi.rescanFiles();
   await db.addAudioFileIndex(afi);
-  // console.log(db.getFlatDatabase());
+  const flat = db.getFlatDatabase();
+  // Some basic stupidity:
+  expect(flat.songs.length).toEqual(735);
+  expect(flat.albums.length).toEqual(187);
+  expect(flat.artists.length).toEqual(271);
+  // More basic stupidity:
+  for (const song of flat.songs) {
+    expect(db.getSong(song.key)).toEqual(song);
+  }
+  for (const album of flat.albums) {
+    expect(db.getAlbum(album.key)).toEqual(album);
+  }
+  // let's find an artist, while we're at it
+  let paulSimon: Artist | undefined = undefined;
+  for (const artist of flat.artists) {
+    expect(db.getArtist(artist.key)).toEqual(artist);
+    if (artist.name.toLocaleLowerCase() === 'paul simon') {
+      paulSimon = artist;
+    }
+  }
+  expect(paulSimon).toBeDefined();
+  expect(paulSimon!.albums.length).toEqual(3);
+  expect(paulSimon!.songs.length).toEqual(5);
+
+  // Now let's do some 'internal consistency' checking
+  // Check for song back-pointers
+  for (const song of flat.songs) {
+    // We can get at the album
+    const album: Album | void = db.getAlbum(song.albumId);
+    expect(album).toBeDefined();
+    if (!album) continue;
+    // The album has this song
+    expect(album.songs.indexOf(song.key)).toBeGreaterThan(-1);
+    for (const artistKey of [...song.artistIds, ...song.secondaryIds]) {
+      // We can get to each artist
+      const artist = db.getArtist(artistKey);
+      expect(artist).toBeDefined();
+      if (!artist) continue;
+      // The artist has this song, too
+      expect(artist.songs.indexOf(song.key)).toBeGreaterThan(-1);
+    }
+  }
+  // Check for album back-pointers
+  for (const album of flat.albums) {
+    // Artist sanity checks
+    if (album.vatype) {
+      expect(album.primaryArtists.length).toEqual(0);
+    } else {
+      for (const artistKey of album.primaryArtists) {
+        const artist = db.getArtist(artistKey);
+        expect(artist).toBeDefined();
+        if (!artist) continue;
+        // The artist has this album
+        expect(artist.albums.indexOf(album.key)).toBeGreaterThan(-1);
+      }
+    }
+    for (const songKey of album.songs) {
+      const song = db.getSong(songKey);
+      expect(song).toBeDefined();
+      if (!song) continue;
+      // The song is on this album
+      expect(song.albumId).toEqual(album.key);
+    }
+  }
+  // And finally, artist back-pointers
+  for (const artist of flat.artists) {
+    for (const songKey of artist.songs) {
+      const song = db.getSong(songKey);
+      expect(song).toBeDefined();
+      if (!song) continue;
+      const pri = song.artistIds.indexOf(artist.key);
+      const alt = song.secondaryIds.indexOf(artist.key);
+      // One of them should be negative
+      expect(pri * alt).toBeLessThanOrEqual(0);
+      // And one should be positive (or zero)
+      expect(pri > alt || alt > pri).toBeTruthy();
+    }
+    for (const albumKey of artist.albums) {
+      const album = db.getAlbum(albumKey);
+      expect(album).toBeDefined();
+      if (!album) continue;
+      const idx = album.primaryArtists.indexOf(artist.key);
+      if (idx < 0) {
+        // If we didn't find this artist as a primary artist
+        // Check to see if one of the songs has the artist
+        const songsInCommon = ArrayIntersection(artist.songs, album.songs);
+        expect(songsInCommon.size).toBeGreaterThan(0);
+      }
+    }
+  }
 });
 
 /*
