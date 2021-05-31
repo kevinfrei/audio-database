8059be9328432688ea5bffb337d330a408b767f5
Kevin Frei <kevinfrei@hotmail.com>
2021-05-31 08:56:17 -0700

    Type Exports, too

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { Song } from '@freik/media-core';
 export * from './AudioDatabase';
-export { GetMediaInfo, IsFullMetadata, IsOnlyMetadata } from './DbMetadata';
+export {
+  GetMediaInfo,
+  IsFullMetadata,
+  IsOnlyMetadata,
+  MinimumMetadata,
+} from './DbMetadata';
 
 export type VAType = '' | 'ost' | 'va';
 export type SongWithPath = Song & { path: string };
