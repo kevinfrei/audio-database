d7e4ac23b4749a1f662ea928b5b5aa0d3f945829
Kevin Frei <kevinfrei@hotmail.com>
2022-11-01 08:58:35 -0700

    More tests, plus yarn offline

--- src/__tests__/tst-helpers.ts
+++ src/__tests__/tst-helpers.ts
@@ -2,12 +2,14 @@ import { Type } from '@freik/core-utils';
 import { promises as fsp } from 'node:fs';
 import rmfr from 'rmfr';
 
+/* istanbul ignore next */
 export async function remove(name: string) {
   try {
     await fsp.rm(name);
   } catch (e) {}
 }
 
+/* istanbul ignore next */
 export async function removeDir(name: string) {
   try {
     await rmfr(name);
